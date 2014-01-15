6b622743600f8e9b3e288aeb8c545fb2c0d35915
drb1001 <pi_anyone@hotmail.com>
2014-01-15 12:21:34 +0100

    added custom palette

--- numbers.R
+++ numbers.R
@@ -1,16 +1,19 @@
 getwd()
+library("RColorBrewer")
 
 mydf  <-  read.csv("train.csv")
 
 head(mydf)
 summary(mydf)
 
+my_palette <- colorRampPalette(c("white", "black"))(n = 50)
+
 shownumber  <- function(n=0, data=mydf) { 
   test  <- as.matrix(mydf[n,][-1], nrow = 28, ncol = 28)
   dim(test)  <-  c(28,28)
-  image(test, main = mydf[n,1] , ylim =c(1,0) )
+  image(test, main = mydf[n,1] , ylim =c(1,0), col = my_palette)
 }
 
-for (mycount1 in 100:130 ) { shownumber(mycount1); Sys.sleep(0.5) }
+for (mycount1 in 100:110 ) { shownumber(mycount1); Sys.sleep(0.5) }
 
 ?image
