a74a724c7f9e59b96b92ee934979a2e57ba49704
drb1001 <pi_anyone@hotmail.com>
2014-09-21 01:42:56 +0200

    small change to file naming

--- submissions/knn/knn.R
+++ submissions/knn/knn.R
@@ -8,8 +8,6 @@ test <- read.csv("Original data sets/test.csv", header=TRUE)
 labels <- train[,1]
 train <- train[,-1]
 
-results <- (0:9)[knn(train, test, labels, k = 10, algorithm="cover_tree")]
+results <- (0:9)[knn(train, test, labels, k = 5, algorithm="cover_tree")]
 
-write(results, file="knn_benchmark.csv", ncolumns=1) 
-
-?knn
+write(results, file="knn_benchmark_k5.csv", ncolumns=1) 
