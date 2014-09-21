14c13bea19796aefd48b5bb1aa9fc2079f4a5b71
drb1001 <pi_anyone@hotmail.com>
2014-09-21 23:22:23 +0200

    corrected knn output format

--- submissions/knn/knn.R
+++ submissions/knn/knn.R
@@ -5,9 +5,17 @@ library(FNN)
 train <- read.csv("Original data sets/train.csv", header=TRUE)
 test <- read.csv("Original data sets/test.csv", header=TRUE)
 
+## for small sample testing of output
+# train <- train[1:1000,]
+# test <- test[1:1000,]
+
 labels <- train[,1]
 train <- train[,-1]
 
-results <- (0:9)[knn(train, test, labels, k = 5, algorithm="cover_tree")]
+results <- data.frame((0:9)[knn(train, test, labels, k = 5, algorithm="cover_tree")])
+names(results) <- c("Label")
+results$ImageId <- 1:nrow(results)
+results <- results[c(2,1)]
 
-write(results, file="knn_benchmark_k5.csv", ncolumns=1) 
+write.csv(results, file="knn_benchmark_k5.csv", quote = FALSE, row.names = FALSE) 
+
