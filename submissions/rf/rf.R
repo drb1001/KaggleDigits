e8dd390dd314f92d06e46924b180d1dda2c47b9d
drb1001 <pi_anyone@hotmail.com>
2014-09-24 17:16:40 +0200

    rerun RF

--- submissions/rf/rf.R
+++ submissions/rf/rf.R
@@ -1,5 +1,7 @@
 # makes the random forest submission
 
+setwd("/Users/danielbrown/Documents/_Personal/Coding projects/R/kaggle/KaggleDigits")
+
 library(randomForest)
 
 train <- read.csv("Original data sets/train.csv", header=TRUE)
@@ -8,11 +10,11 @@ test <- read.csv("Original data sets/test.csv", header=TRUE)
 labels <- as.factor(train[,1])
 train <- train[,-1]
 
-rf <- randomForest(train, labels, xtest=test, ntree=20)
+rf <- randomForest(train, labels, xtest=test, ntree=1000)
 predictions <- data.frame(levels(labels)[rf$test$predicted])
 
 names(predictions) <- c("Label")
 predictions$ImageId <- 1:nrow(predictions)
 predictions <- predictions[c(2,1)]
 
-write.csv(predictions, file="rf_20.csv", quote = FALSE, row.names = FALSE) 
+write.csv(predictions, file="rf_1000.csv", quote = FALSE, row.names = FALSE) 
